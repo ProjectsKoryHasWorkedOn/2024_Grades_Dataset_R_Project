655c20fbfaf4d8cf0be958592d315f52d5cc00f7
Kory <kory@LAPTOP-LCCT68U5.localdomain>
2024-03-21 22:04:42 +1030

    remove nested list

--- r_project.R
+++ r_project.R
@@ -4,6 +4,7 @@
 library(mongolite)
 library(tidyverse) # contains ggplot, etc.
 library(cowplot)
+library(jsonlite)
 
 #Connect to Mongo DB
 connection_string = 'mongodb+srv://fran0618:WhwdV2u7cMmUJuDj@comp2031-8031.hfcmwd0.mongodb.net/?retryWrites=true&w=majority&appName=COMP2031-8031' # Kory's connection string
@@ -21,34 +22,53 @@ dbConnection$iterate()$one() # iterate through elements by increments of one
 # 'student_id'
 
 # Getting specific data from DB. 
-query<-list()
+# query<-list()
 # query[["student_id"]][["$gt"]] <- 0 # {all student IDs}
-query[["student_id"]] <- 0 # {student id of 0}
-query[["class_id"]] <- 339 # {class id of 339}
-query <- jsonlite::toJSON(query, auto_unbox = TRUE) # Convert this to JSON format
-query
+# query[["student_id"]] <- 0 # {student id of 0}
+# query[["class_id"]] <- 339 # {class id of 339}
+# query <- jsonlite::toJSON(query, auto_unbox = TRUE) # Convert this to JSON format
+# query
 
 # Putting this data into a data frame
-df <- as.data.frame(dbConnection$find(query))
-head(df)
+# df <- as.data.frame(dbConnection$find(query))
+df <- as.data.frame(dbConnection$find())
+# removenesting <- df %>% select(scores) %>% tidyr::unnest(scores)
+# head(removenesting, 20)
 
-student_id_vector <- as.integer(unlist(df$student_id))
+df <- df %>% tidyr::unnest(scores)
+# df <- df %>% select(scores) %>% tidyr::unnest(scores)
+
+view(df)
+
+sumIsNa = sum(is.na(df))
+sumIsNa
+
+# tibble <- as_tibble(df)
+#view(df)
 
 
-scores_vector <- as.integer(unlist(df$scores))
 
-MISSING <- is.na(scores_vector)
-scores_vector_no_na <- subset(scores_vector, 
-                              subset = !MISSING)
+# head(df)
+# use view to see it all, cause it doesn't show in output
+# too big
 
-scores_vector_no_na
+# student_id_vector <- as.integer(unlist(df$student_id))
 
-ggplot(mapping = aes(x = student_id_vector, y = scores_vector))  + 
-  geom_point()
+
+#scores_vector <- as.integer(unlist(df$scores))
+
+# MISSING <- is.na(scores_vector)
+# scores_vector_no_na <- subset(scores_vector, 
+  #                            subset = !MISSING)
+
+# scores_vector_no_na
+
+# ggplot(mapping = aes(x = student_id_vector, y = scores_vector))  + 
+  # geom_point()
 
 
 
 # Performing some basic calculations
-mean(scores_vector_no_na)
-median(scores_vector_no_na)
+# mean(scores_vector_no_na)
+# median(scores_vector_no_na)
 
