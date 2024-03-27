5d16aaab9c45e944aa2cfdef4fae435776cc73b2
Kory <39051012+ProjectsKoryHasWorkedOn@users.noreply.github.com>
2024-03-28 00:37:27 +1030

    Delete r_libraries_to_install.R

--- r_libraries_to_install.R
+++
@@ -1,6 +0,0 @@
-#R libraries - Install them once then don't run again
-
-#To install
-install.packages("mongolite") # MongoDB
-install.packages("tidyverse", dependencies=T) # has a number of packages inside of it
-install.packages("cowplot")
